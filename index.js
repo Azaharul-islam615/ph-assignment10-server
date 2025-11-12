f1aa22c504f959c6f70105606117d683d5b68cf5
Azaharul-islam615 <mdazaharul725@gmail.com>
2025-11-12 23:18:24 +0600

    add environment variable

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express=require ('express')
 const cors=require('cors')
+require('dotenv').config()
+
 const port=process.env.PORT || 3000
 const app=express()
 app.use(cors())
@@ -8,7 +10,7 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('server is running')
 })
-const uri = "mongodb+srv://assignment10:m80eqehahCN0Goe8@cluster0.jckqi5e.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jckqi5e.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
